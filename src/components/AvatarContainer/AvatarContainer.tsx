e92be251d85a2a44b983bbbc07310a2429e4a5e7
jielim36 <jielim00@gmail.com>
2024-06-06 10:48:47 +0800

    add customize avatar container classname props and put a default size make it more smaller.

--- src/components/AvatarContainer/AvatarContainer.tsx
+++ src/components/AvatarContainer/AvatarContainer.tsx
@@ -6,13 +6,14 @@ type AvatarContainerProps = {
     avatar_url?: string;
     hasStory: boolean;
     className?: string;
+    avatarClassName?: string;
 }
 
-const AvatarContainer: React.FC<AvatarContainerProps> = ({ avatar_url, hasStory, className }) => {
+const AvatarContainer: React.FC<AvatarContainerProps> = ({ avatar_url, hasStory, className, avatarClassName }) => {
     return (
         <div className={`${hasStory ? "bg-gradient-2" : ""} p-[2px] rounded-full ${className}`}>
             <div className="rounded-full p-[1px] card-color">
-                <Avatar>
+                <Avatar className={`w-9 h-9 ${avatarClassName}`}>
                     <AvatarImage src={avatar_url} />
                     <AvatarFallback>
                         <UserRoundIcon className="text-slate-500" />
