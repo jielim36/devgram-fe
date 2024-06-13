feb774ea4b26c83f5ece3268c6cd17c9b3c19799
jielim36 <jielim00@gmail.com>
2024-06-13 17:37:30 +0800

    Add new props userId for click avatar icon and jump to specific user profile page

--- src/components/AvatarContainer/AvatarContainer.tsx
+++ src/components/AvatarContainer/AvatarContainer.tsx
@@ -3,6 +3,7 @@ import "@/style/color.css"
 import Icon from "../Icon/Icon";
 
 type AvatarContainerProps = {
+    userId?: number;
     avatar_url?: string;
     hasStory: boolean;
     className?: string;
@@ -10,13 +11,32 @@ type AvatarContainerProps = {
     boldBorder?: boolean;
 }
 
-const AvatarContainer: React.FC<AvatarContainerProps> = ({ avatar_url, hasStory, className, avatarClassName, boldBorder = false }) => {
+const AvatarContainer: React.FC<AvatarContainerProps> = ({
+    avatar_url,
+    hasStory,
+    className,
+    avatarClassName,
+    boldBorder = false,
+    userId
+}) => {
 
     const padding = boldBorder ? "p-[4px]" : "p-[2px]";
     const childPadding = boldBorder ? "p-[2px]" : "p-[1px]";
+    const userProfilePath = `/profile/${userId}`;
+
+    const handleAvatarClick = () => {
+        if (hasStory) {
+            // pop up story
+        } else if (userId) {
+            window.location.href = userProfilePath;
+        }
+    }
 
     return (
-        <div className={`${hasStory ? "bg-gradient-2" : ""} ${padding} rounded-full cursor-pointer ${className}`}>
+        <div
+            className={`${hasStory ? "bg-gradient-2" : ""} ${padding} rounded-full cursor-pointer ${className}`}
+            onClick={handleAvatarClick}
+        >
             <div className={`rounded-full ${childPadding} card-color`}>
                 <Avatar className={`w-9 h-9 ${avatarClassName}`}>
                     <AvatarImage src={avatar_url} />
