8da6723885805bb2ff031e2c8851390efc96b886
jielim36 <jielim00@gmail.com>
2024-06-24 14:00:23 +0800

    Applied memo to Icon component to avoid re-render icon when other component changing such as input field

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, memo, Suspense } from 'react';
 import { LucideProps } from 'lucide-react';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
 
@@ -8,16 +8,18 @@ interface IconProps extends Omit<LucideProps, 'ref'> {
     name: keyof typeof dynamicIconImports;
 }
 
-const fallback = <div style={{ background: '#ddd', width: 24, height: 24 }} />
+const fallback = <div style={{ background: 'transparent', width: 24, height: 24 }} />
 
-const Icon = ({ name, ...props }: IconProps) => {
+const IconComponent = ({ name, ...props }: IconProps) => {
     const LucideIcon = lazy(dynamicIconImports[name]);
 
     return (
-        <Suspense fallback={<div></div>}>
+        <Suspense fallback={fallback}>
             <LucideIcon {...props} />
         </Suspense>
     );
 }
 
-export default Icon
+const Icon = memo(IconComponent);
+
+export default Icon;
