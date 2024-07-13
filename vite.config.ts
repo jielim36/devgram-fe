7a5803c3451e74a9edd0d258b99c836611a7a894
jielim36 <jielim00@gmail.com>
2024-07-13 14:37:52 +0800

    Completed run application with different environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,49 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+export default defineConfig(({ mode }) => {
+
+  dotenv.config({
+    path: `.env.${mode}`
+  });
+
+  const env = loadEnv(mode, process.cwd());
+  const port: number = parseInt(env.VITE_PORT);
+
+  console.log("MODE: ", mode);
+  console.log("PORT: ", port);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: port,
     },
-  },
-})
+    preview: {
+      port: port,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      // 'process.env': env
+      'process.env.VITE_PORT': JSON.stringify(env.VITE_PORT),
+    }
+  };
+});
