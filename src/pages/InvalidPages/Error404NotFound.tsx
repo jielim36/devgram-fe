eb0a56cbc1500cf4375025ee7f313d8d90d9f6fd
jielim36 <jielim00@gmail.com>
2024-08-01 13:42:37 +0800

    Updated error page.

--- src/pages/InvalidPages/Error404NotFound.tsx
+++ src/pages/InvalidPages/Error404NotFound.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
 const Error404NotFound = () => {
+
+    const handleBack = () => {
+        history.back();
+    }
+
     return (
         <Card className="w-screen h-screen flex justify-center items-center">
-            <span className="text-[200px]">404 Not Found</span>
+            <span className="text-[60px]">404 Not Found</span>
+            <Button onClick={handleBack}>Back</Button>
         </Card>
     );
 }
